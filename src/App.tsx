d5c7c7f9c08abe2f9ac5ef2d3364f71d9e613569
95decode <95decode@gmail.com>
2023-01-09 00:08:40 +0900

    `Update gh-pages`

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<Connect />} />
                     <Route path="*" element={<NotFound />} />
